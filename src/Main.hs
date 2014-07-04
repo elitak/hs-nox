af551362303e5cd35a56435ed09f7ddb859eb327
Eric Litak <elitak@gmail.com>
2014-07-03 18:24:36 -0700

    well, it builds now, at least

---
+++ src/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Nox.Network.WireProtocol
+import Nox.Crypt
+
+main = do
+    putStrLn "noxd v0.1" -- v1.0 will be all original nox msgs supported, if not game modes (like Quest)
