8a0bf61783a6909b82c719b7a688a8f82a772e6f
Eric Litak <elitak@gmail.com>
2014-09-09 18:08:07 -0700

    a good start on compression

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Nox.Network.WireProtocol
 import Nox.Crypt
+import Nox.Zip
 
 import Data.Conduit
 import Data.Conduit.Binary
@@ -11,4 +12,10 @@ import Control.Monad.Trans.Resource (runResourceT)
 --      + conduit-cereal??
 main = do
     putStrLn "noxd v0.1" -- v1.0 will be all original nox msgs supported, if not game modes (like Quest)
-    runResourceT $ sourceFile "gamedata.bin" $= decrypt GameData $$ sinkFile "gamedata.out"
+    --runResourceT $ sourceFile "gamedata.bin" $= decrypt GameData $$ sinkFile "gamedata.txt"
+    --runResourceT $ sourceFile "So_Beach.map" $= decrypt Map $$ sinkFile "map.out"
+    runResourceT $ sourceFile "So_Beach.map" $= compress $$ sinkFile "map.out"
+
+
+-- TODO Test performance early and often, so that I can make sure the server binary runs as-fast or faster than original in terms of netcode/responsiveness
+-- In particular, I worry that an elegant, Alternative-based (de)serializer might be slow (or whatever solution I pick).
