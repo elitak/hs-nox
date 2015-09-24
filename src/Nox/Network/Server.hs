18b9e23f8eff4179089b019847156aa5d20fcf32
Eric Litak <elitak@gmail.com>
2015-09-24 08:25:56 -0700

    mvar and config type tmp

--- src/Nox/Network/Server.hs
+++ src/Nox/Network/Server.hs
@@ -12,14 +12,35 @@ import Data.ByteString
 import qualified Data.ByteString.Char8 as BC
 import Data.Serialize
 import Data.Flags
+import Data.Yaml
+import Control.Concurrent hiding (yield)
+import Control.Monad.IO.Class (liftIO)
+import Data.Monoid
 
 import Nox.Network.WireProtocol
+import Nox.Network.WireProtocol.ServerInfo
 
 gamePort = 18590
 maxPayload = 1500
 
+data Config = Config { serverStatus :: ServerStatus
+                     }
+
+data ServerStatus = ServerStatus { bannedSpells :: [AllowedSpells]
+                                 }
+
+instance Monoid AllowedSpells where
+    mempty = noFlags
+    mappend = (.+.)
+
+refreshConfig confVar = do
+    let newConf = Config { serverStatus = ServerStatus { bannedSpells = [lightning] } }
+    putMVar confVar newConf
+
 noxd :: IO ()
 noxd = withSocketsDo $ do
+    confVar <- newEmptyMVar
+    refreshConfig confVar
     sock <- socket AF_INET Datagram 0
     bindSocket sock (SockAddrInet gamePort iNADDR_ANY)
     -- TODO Insert xor code tinto this pipeline somewhere for transparent ciphering.
@@ -27,7 +48,7 @@ noxd = withSocketsDo $ do
     --let loop sock = (sourceSocket sock maxPayload $= decipher =$= handleMsg =$= encipher $$ sinkToSocket sock) >> loop sock
     -- TODO Am i even getting anything out of using conduits here?
     let loop s = ( sourceSocket s maxPayload
-                $= handleMsg
+                $= handleMsg confVar
                 $$ sinkToSocket s
                  ) >> loop s
     loop sock
@@ -39,7 +60,9 @@ noxd = withSocketsDo $ do
 --        Just msg@Message{..} -> trace (show msgData) $ yield msg
 --        Nothing -> return ()
 
-handleMsg = do
+handleMsg confVar = do
+    -- XXX really re-read config every loop??
+    conf <- liftIO $ readMVar confVar
     mbMsg <- await
     case mbMsg of
         Just Message{..} -> do
@@ -78,7 +101,7 @@ handleMsg = do
                                                           , 0x00
 	                                                  , 0xD4
                                                           , 0x00]
-                                          , okSpells = allFlags
+                                          , okSpells = allBut (mconcat (bannedSpells . serverStatus $ conf))
                                           , unkBS2 = pack [
                                                             0xFF --notmask?
                                                           , 0xFF --notmask
