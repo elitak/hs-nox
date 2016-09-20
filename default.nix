59a3b54f4a268cc4de48b77e007b40cf5cb6b915
Eric Litak <elitak@gmail.com>
2016-09-19 22:16:57 -0700

    nixification

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ nixpkgs ? import ../nixpkgs/actual { }
+, compiler ? "default"
+}:
+let
+  inherit (nixpkgs) pkgs;
+  haskellPackages = if compiler == "default" then pkgs.haskellPackages else pkgs.haskell.packages.${compiler};
+in
+with pkgs;
+with pkgs.lib;
+overrideDerivation (haskellPackages.callPackage ./package.nix { }) (oldAttrs: { preferLocalBuild = true; })
