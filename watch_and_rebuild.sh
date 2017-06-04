a6d22a0f37bfba51432fa7f83c362d9cbdf3619d
Eric Litak <elitak@gmail.com>
2017-06-04 03:24:17 -0700

    improved rebuild

--- watch_and_rebuild.sh
+++ watch_and_rebuild.sh
@@ -1,6 +1,25 @@
 #! /usr/bin/env bash
+
+target="./dist/build/noxd/noxd"
+
+trap cleanup SIGINT
+
+cleanup() {
+        echo "Cleaning up..."
+        pkill "$target"
+        exit
+}
+
+# Start the daemon once, on initial run, if possible
+[[ -x "$target" ]] && "$target" &
+
 while true; do
-        inotifywait -qe modify $(find . -iname \*.hs)
+        IFS=$'\n' sourceFiles=($(find . -iname \*.hs))
+        inotifywait -qe modify "${sourceFiles[@]}"
+
+        # Do not leave daemon running, in the case of build failure. This is
+        # more sensible than leaving a version running that doesn't have the
+        # changes we expected to be effected.
         pkill noxd
-        cabal build && ./dist/build/noxd/noxd &
+        cabal build && "$target" &
 done
