726f0efff1308a685eaa8899a57bc5ca7b741030
Eric Litak <elitak@gmail.com>
2018-10-30 01:52:59 -0700

    cped default from skeleton

--- default.nix
+++ default.nix
@@ -1,26 +1,20 @@
 { pkgs ? import <nixpkgs> { }
 , compiler ? "default"
 }:
+with pkgs; with pkgs.lib; with builtins;
 let
-  inherit (builtins) filterSource;
-  inherit (pkgs.lib.lists) any;
-  inherit (pkgs.lib.strings) hasPrefix hasSuffix substring stringLength;
   haskellPackages = if compiler == "default" then pkgs.haskellPackages else pkgs.haskell.packages.${compiler};
-  pkgFile = ./package.nix;
-  include = includedFiles: src: filterSource (candidatePath: type:
-    any (pattern:
-      let fullPath = toString (src + ("/" + pattern)); in
-      candidatePath == fullPath || # literal file name match
-      #type == "directory" && hasPrefix fullPath candidatePath ||
-      type == "directory" || # all directories
-      hasPrefix "*" pattern && hasSuffix (substring 1 (stringLength pattern) pattern) candidatePath
+  pkgFile = ./package.nix; # nix-shell -p cabal2nix --pure --run 'cabal2nix . >package.nix'
+  include = includedFiles: src: filterSource (path: type:
+    lists.any (f:
+      let p = toString (src + ("/" + f)); in
+      (path == p) || (type == "directory" && strings.hasPrefix path p)
     ) includedFiles
   ) src;
 in (haskellPackages.callPackage ./package.nix {}).overrideDerivation (old: {
   src = include [
-    "hs-nox.cabal"
-    #"src" "*.hs" # FIXME: this dir includes all subdirs and *.hs doesnt work without it. "src/*.hs" should do both?
-    "*.hs"
+    "thisproject.cabal"
+    "src"
   ] old.src;
   preferLocalBuild = true;
 })
