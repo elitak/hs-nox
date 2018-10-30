3c34c67199701cdfdd8bc71bd927941f7eeaed25
Eric Litak <elitak@gmail.com>
2018-10-30 01:52:38 -0700

    prly trash changes

--- default.nix
+++ default.nix
@@ -1,10 +1,26 @@
-{ nixpkgs ? import <nixpkgs> { }
-, compiler ? "ghc7102"
+{ pkgs ? import <nixpkgs> { }
+, compiler ? "default"
 }:
 let
-  inherit (nixpkgs) pkgs;
+  inherit (builtins) filterSource;
+  inherit (pkgs.lib.lists) any;
+  inherit (pkgs.lib.strings) hasPrefix hasSuffix substring stringLength;
   haskellPackages = if compiler == "default" then pkgs.haskellPackages else pkgs.haskell.packages.${compiler};
-in
-with pkgs;
-with pkgs.lib;
-overrideDerivation (haskellPackages.callPackage ./package.nix { }) (oldAttrs: { preferLocalBuild = true; })
+  pkgFile = ./package.nix;
+  include = includedFiles: src: filterSource (candidatePath: type:
+    any (pattern:
+      let fullPath = toString (src + ("/" + pattern)); in
+      candidatePath == fullPath || # literal file name match
+      #type == "directory" && hasPrefix fullPath candidatePath ||
+      type == "directory" || # all directories
+      hasPrefix "*" pattern && hasSuffix (substring 1 (stringLength pattern) pattern) candidatePath
+    ) includedFiles
+  ) src;
+in (haskellPackages.callPackage ./package.nix {}).overrideDerivation (old: {
+  src = include [
+    "hs-nox.cabal"
+    #"src" "*.hs" # FIXME: this dir includes all subdirs and *.hs doesnt work without it. "src/*.hs" should do both?
+    "*.hs"
+  ] old.src;
+  preferLocalBuild = true;
+})
